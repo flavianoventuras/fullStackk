334e584da54c0897ddcfd88031e9d1ef49cb3cf7
flavianoventuras <flavianoventuras@hotmail.com>
2024-06-13 20:22:32 -0300

    terceiro envio

--- modulo_externo/index.js
+++ modulo_externo/index.js
@@ -1,12 +1,19 @@
-//Core Module URL
+// Unir HTTP e URL
 
-const url = require('url');
-
-const address = "http://www.meusite.com.br/catalog?produtos=cadeira"
-
-const parseURL = new url.URL(address);
-]console.log(parseURL.host);
-]console.log(parseURL.pathname);
-]console.log(parseURL.search);
-]console.log(parseURL.searchParams);
-]console.log(parseURL.searchParams.get('produtos'));
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) =>{
+    
+    const urlInfo = require('url').parse(req.url, true)
+    const name = urlInfo.query.name;
+    res.setHeader("Content-Type", "text/html");
+    if(!name){
+        res.end(`<h1>Preencha o seu nome</h1><form method="GET"><input type="text" name="name"><input type="submit" value="Enviar"></form>`)
+        
+    }else{
+        res.end(`<h1> Seja bem-vindo, ${name}!! </h1>`)
+    }  
+})
+server.listen(port, ()=>{
+    console.log('Servidor rodando na porta: ', port)
+})
