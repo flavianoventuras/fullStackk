f7c6623dc76c284b23b2ada842964f59df7cd59f
flavianoventuras <flavianoventuras@hotmail.com>
2024-06-14 19:27:50 -0300

    Excluir arquivos

--- modulo_externo/index.js
+++ modulo_externo/index.js
@@ -1,24 +1,5 @@
-const http = require('http');
-    const fs = require('fs');
+const fs = require('fs');
 
-    const port = 3000;
-    const server = http.createServer((req, res) =>{
-        const urlInfo = require("url").parse(req.url, true);
-        const name = urlInfo.query.name;
-        if(!name){
-            fs.readFile("index.html", (err, data)=>{
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.write(data);
-                res.end();
-        })
-        } else{
-
-            const nameNewLine = name + '\r\n';
-            fs.appendFile("arquivo.txt", nameNewLine, function(err, data){
-                    res.writeHead(302, {location: "/"})
-                    res.end()
-            })}   
-    })   
-    server.listen(port, ()=>{
-        console.log('Servidor rodando na porta: ', port)
-    })
+fs.unlink("arquivo.txt", (err) =>{
+    console.log(err);
+})
