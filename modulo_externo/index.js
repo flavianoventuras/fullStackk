ae28ef45b45dea9aab2d7a8eb3e53dda530bb933
flavianoventuras <flavianoventuras@hotmail.com>
2024-06-14 19:14:38 -0300

    Salva nome no arquivotxt

--- modulo_externo/index.js
+++ modulo_externo/index.js
@@ -1,18 +1,24 @@
-// Unir HTTP e URL
-
 const http = require('http');
+    const fs = require('fs');
 
-const fs = require('fs');
+    const port = 3000;
+    const server = http.createServer((req, res) =>{
+        const urlInfo = require("url").parse(req.url, true);
+        const name = urlInfo.query.name;
+        if(!name){
+            fs.readFile("index.html", (err, data)=>{
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.write(data);
+                res.end();
+        })
+        } else{
 
-const port = 3000;
-const server = http.createServer((req, res) =>{
-    fs.readFile("mensagem.html", (err, data)=>{
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.write(data);
-        res.end();
+            const nameNewLine = name + '\r\n';
+            fs.appendFile("arquivo.txt", nameNewLine, function(err, data){
+                    res.writeHead(302, {location: "/"})
+                    res.end()
+            })}   
+    })   
+    server.listen(port, ()=>{
+        console.log('Servidor rodando na porta: ', port)
     })
-})
-
-server.listen(port, ()=>{
-    console.log('Servidor rodando na porta: ', port)
-})
