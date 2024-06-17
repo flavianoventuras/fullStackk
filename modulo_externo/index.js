fd450b583c260e5f44bc79f9fe4d5cc700809f93
flavianoventuras <flavianoventuras@hotmail.com>
2024-06-17 19:26:57 -0300

    Criando pasta com NODE.JS

--- modulo_externo/index.js
+++ modulo_externo/index.js
@@ -1,7 +1,10 @@
-const path = require('path');
+const fs = require('fs');
 
-const CaminhoCriadorDePasta = "/relatorio/tiagorelatorios/relatorio1.pdf"
-
-console.log(path.dirname(CaminhoCriadorDePasta));
-console.log(path.basename(CaminhoCriadorDePasta));
-console.log(path.extname(CaminhoCriadorDePasta));
+if(fs.existsSync('./minhaPasta')){
+    console.log('Este diretório já existe')
+} else {
+    console.log("Criando diretório...")
+    fs.mkdir('minhaPasta', (err)=>{
+        console.log(err);
+    });
+}
