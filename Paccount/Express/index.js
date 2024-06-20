93bf4afae929aed6817daca0de597d1c77ed8dfa
flavianoventuras <flavianoventuras@hotmail.com>
2024-06-20 20:23:36 -0300

    Next Linhas 15,18

--- Paccount/Express/index.js
+++ Paccount/Express/index.js
@@ -7,6 +7,20 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
+const checkAuth = function(req, res, next){
+    req.authStatus = true;
+
+    if(req.authStatus){
+        console.log("Está logado")
+        next(0);
+    } else {
+        console.log("Não está logado")
+        next();
+    }
+}
+
+app.use(checkAuth);
+
 app.get('/', (req, res)=>{ //req = requisição, são os dados coletados  //res = a gente enviar
     // res.send('Ola mundo')
     res.sendFile(`${basePath}/index.html`)
